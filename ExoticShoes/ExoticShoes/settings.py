5fd250e1834263c89fea9c591171fd582152d5d6
Juan David <dajun318@gmail.com>
2023-06-26 11:41:17 -0500

    Changes

--- ExoticShoes/ExoticShoes/settings.py
+++ ExoticShoes/ExoticShoes/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR,'/static/')
-#para guardar archivos de multimedia
+
+
+#para guardar archivos de multimedia 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
