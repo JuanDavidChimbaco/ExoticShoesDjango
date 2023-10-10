1c96854d404496cb885a9a017fac2ac12b424bcb
Juan David <dajun318@gmail.com>
2023-10-10 10:21:02 -0500

    changes banner

--- ExoticShoes/appExoticShoes/static/js/carousel.js
+++ ExoticShoes/appExoticShoes/static/js/carousel.js
@@ -9,13 +9,42 @@ function configCarousel() {
 
 async function ultimosProductos(){
     try {
-        url = '/api/v1.0/'
-        const response = await axios.get('')
+        url = '/api/v1.0/categoriaCliente'
+        const response = await axios.get(url)
+        const categorias = response.data;
+
+        // Obtén la referencia al elemento del carrusel
+        const carouselInner = document.querySelector('.carousel-inner');
+
+        // Recorre las categorías y crea elementos de imagen para cada una
+        categorias.forEach((categoria, index) => {
+            const carouselItem = document.createElement('div');
+            carouselItem.classList.add('carousel-item');
+
+            const imgContainer = document.createElement('div');
+            imgContainer.classList.add('d-flex', 'w-100', 'justify-content-center');
+
+            const img = document.createElement('img');
+            img.src = categoria.imagen; // Asume que la categoría tiene una propiedad "imagen_url"
+            img.classList.add('rounded', 'img-fluid', 'categoria-imagen');
+            img.alt = `Imagen de ${categoria.nombre}`; // Asume que la categoría tiene una propiedad "nombre"
+
+            imgContainer.appendChild(img);
+            carouselItem.appendChild(imgContainer);
+
+            if (index === 0) {
+                carouselItem.classList.add('active');
+            }
+
+            // carouselInner.appendChild(carouselItem);
+        });
+
+        console.log('categorias', categorias);
     } catch (error) {
-        
+        console.error(error);
     }
 }
-
 window.onload = () => {
     configCarousel();
+    ultimosProductos();
 };
